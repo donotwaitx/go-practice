0aa7e4758863669b803b9f6118d4992c4eaef950
lehoangtrong <lehoangtrongcsgl@gmail.com>
2026-05-19 15:48:15 +0700

    feat(basics): 01 - hello world

---
+++ basics/01-hello/main.go
@@ -0,0 +1,10 @@
+// 01 - Hello World
+// Mọi chương trình Go chạy được đều bắt đầu từ package main và hàm main().
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, Go!")
+	fmt.Printf("Đây là %s, phiên bản %d\n", "Go", 1)
+}
